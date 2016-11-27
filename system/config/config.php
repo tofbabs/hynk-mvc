a7c86aaa893226ad562bde342d5a3b29f51a08d3
tofbabs <tofunmibabatunde@gmail.com>
2016-11-27 09:10:53 +0100

    ReadMe added

---
+++ system/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+date_default_timezone_set('Africa/Lagos');
+
+define('DEVELOPMENT_ENVIRONMENT', true);
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_ACTION', 'index');
+
+define('BASE_PATH', '/Users/Tofunmi/Sites/hynk-mvc/');
+define('ROOT_URL', 'http://localhost/hynk-mvc/');
+
+define('NOT_FOUND_CONTROLLER','index');
+
+define('DB_HOST','127.0.0.1');
+define('DB_NAME','campaign_manager');
+define('DB_USER','root');
+define('DB_PASS','fileopen');
+
+
+?>
